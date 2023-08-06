685e64ae7c6a341b97233d1d9e66b87775603109
Roger John <109475028+rojergohn@users.noreply.github.com>
2023-08-06 13:10:13 +0530

    Add files via upload

---
+++ Tables.py.py
@@ -0,0 +1,31 @@
+a = int(input('Type the Table 2 to 10 you want . '))
+
+if a == 2:
+    for i in range(1,10+1):
+        print(f'{i} X 2 = {i*a}')
+elif a == 3:
+    for i in range(1,10+1):
+        print(f'{i} X 3 = {i*a}')
+elif a == 4:
+    for i in range(1,10+1):
+        print(f'{i} X 4 = {i*a}')
+elif a == 5:
+    for i in range(1,10+1):
+        print(f'{i} X 5 = {i*a}')
+elif a == 6:
+    for i in range(1,10+1):
+        print(f'{i} X 6 = {i*a}')
+elif a == 7:
+    for i in range(1,10+1):
+        print(f'{i} X 7 = {i*a}')
+elif a == 8:
+    for i in range(1,10+1):
+        print(f'{i} X 8 = {i*a}')
+elif a == 9:
+    for i in range(1,10+1):
+        print(f'{i} X 9 = {i*a}')
+elif a == 10:
+    for i in range(1,10+1):
+        print(f'{i} X 10 = {i*a}')
+else:
+    print('Invalid number')
